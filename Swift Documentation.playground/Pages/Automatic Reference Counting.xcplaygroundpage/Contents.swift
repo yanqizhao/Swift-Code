24802663ea38f3468d59649c017f88e2a94f63df
yanqizhao <yanqizhao3@gmail.com>
2022-03-28 20:28:36 +0800

    feat: documentation automatic reference counting

---
+++ Swift Documentation.playground/Pages/Automatic Reference Counting.xcplaygroundpage/Contents.swift
@@ -0,0 +1,207 @@
+//: [Previous](@previous)
+
+import Foundation
+
+/**
+ 弱引用
+ */
+class Person {
+    let name: String
+    init(name: String) { self.name = name }
+    var apartment: Apartment?
+    deinit { print("\(name) is being deinitialized") }
+}
+
+class Apartment {
+    let unit: String
+    init(unit: String) { self.unit = unit }
+    weak var tenant: Person?
+    deinit { print("Apartment \(unit) is being deinitialized") }
+}
+
+var john: Person?
+var unit4A: Apartment?
+
+john = Person(name: "John Appleseed")
+unit4A = Apartment(unit: "4A")
+
+john!.apartment = unit4A
+unit4A!.tenant = john
+
+john = nil
+// 打印“John Appleseed is being deinitialized”
+
+unit4A = nil
+// 打印“Apartment 4A is being deinitialized”
+
+
+/**
+ 无主引用
+ */
+var greeting = "Hello, playground"
+
+class Customer {
+    let name: String
+    var card: CreditCard?
+    init(name: String) {
+        self.name = name
+    }
+    deinit { print("\(name) is being deinitialized") }
+}
+
+class CreditCard {
+    let number: UInt64
+    unowned let customer: Customer
+    init(number: UInt64, customer: Customer) {
+        self.number = number
+        self.customer = customer
+    }
+    deinit { print("Card #\(number) is being deinitialized") }
+}
+
+var john2: Customer?
+john2 = Customer(name: "John2 Appleseed")
+john2!.card = CreditCard(number: 1234_5678_9012_3456, customer: john2!)
+john2 = nil
+// 打印“John Appleseed is being deinitialized”
+// 打印“Card #1234567890123456 is being deinitialized”
+
+
+/**
+ 无主可选引用
+ */
+class Department {
+    var name: String
+    var courses: [Course]
+    init(name: String) {
+        self.name = name
+        self.courses = []
+    }
+}
+
+class Course {
+    var name: String
+    unowned var department: Department
+    unowned var nextCourse: Course?
+    init(name: String, in department: Department) {
+        self.name = name
+        self.department = department
+        self.nextCourse = nil
+    }
+}
+
+let department = Department(name: "Horticulture")
+
+let intro = Course(name: "Survey of Plants", in: department)
+let intermediate = Course(name: "Growing Common Herbs", in: department)
+let advanced = Course(name: "Caring for Tropical Plants", in: department)
+
+intro.nextCourse = intermediate
+intermediate.nextCourse = advanced
+department.courses = [intro, intermediate, advanced]
+
+
+/**
+ 无主引用和隐式解包可选项属性
+ */
+class Country {
+    let name: String
+    var capitalCity: City!
+    init(name: String, capitalName: String) {
+        self.name = name
+        self.capitalCity = City(name: capitalName, country: self)
+    }
+}
+
+class City {
+    let name: String
+    unowned let country: Country
+    init(name: String, country: Country) {
+        self.name = name
+        self.country = country
+    }
+}
+
+var country = Country(name: "Canada", capitalName: "Ottawa")
+print("\(country.name)'s capital city is called \(country.capitalCity.name)")
+// 打印“Canada's capital city is called Ottawa”
+
+
+/**
+ 闭包的循环强引用
+ */
+class HTMLElement {
+
+    let name: String
+    let text: String?
+
+    lazy var asHTML: () -> String = {
+        if let text = self.text {
+            return "<\(self.name)>\(text)</\(self.name)>"
+        } else {
+            return "<\(self.name) />"
+        }
+    }
+    
+    init(name: String, text: String? = nil) {
+        self.name = name
+        self.text = text
+    }
+    
+    deinit {
+        print("\(name) is being deinitialized")
+    }
+    
+}
+
+let heading = HTMLElement(name: "h1")
+let defaultText = "some default text"
+heading.asHTML = {
+    return "<\(heading.name)>\(heading.text ?? defaultText)</\(heading.name)>"
+}
+print(heading.asHTML())
+// 打印“<h1>some default text</h1>”
+
+var paragraph: HTMLElement? = HTMLElement(name: "p", text: "hello, world")
+print(paragraph!.asHTML())
+// 打印“<p>hello, world</p>”
+
+paragraph = nil
+
+
+/**
+ 解决闭包的循环强引用
+ */
+class HTMLElement2 {
+
+    let name: String
+    let text: String?
+
+    lazy var asHTML: () -> String = {
+        [unowned self] in
+        if let text = self.text {
+            return "<\(self.name)>\(text)</\(self.name)>"
+        } else {
+            return "<\(self.name) />"
+        }
+    }
+
+    init(name: String, text: String? = nil) {
+        self.name = name
+        self.text = text
+    }
+
+    deinit {
+        print("\(name)2 is being deinitialized")
+    }
+
+}
+
+var paragraph2: HTMLElement2? = HTMLElement2(name: "p", text: "hello, world")
+print(paragraph2!.asHTML())
+// 打印“<p>hello, world</p>”
+
+paragraph2 = nil
+// 打印“p is being deinitialized”
+
+//: [Next](@next)
