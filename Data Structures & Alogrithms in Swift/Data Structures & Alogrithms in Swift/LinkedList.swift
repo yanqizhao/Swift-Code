b42a7d12af1b1e304ec23e7631bc462f1c11023d
yanqizhao <yanqizhao3@gmail.com>
2022-04-28 17:11:03 +0800

    feat:  Data Structures & Alogrithms in Swift LinkedList

---
+++ Data Structures & Alogrithms in Swift/Data Structures & Alogrithms in Swift/LinkedList.swift
@@ -0,0 +1,268 @@
+public struct LinkedList<Value> {
+    public var head: Node<Value>?
+    public var tail: Node<Value>?
+    
+    // 为了公开，默认是 internal
+    public init() {}
+    
+    public var isEmpty: Bool {
+        head == nil
+    }
+    
+    public func node(at index: Int) -> Node<Value>? {
+        var currentNode = head
+        var currentIndex = 0
+        
+        while currentNode != nil && currentIndex < index {
+            currentNode = currentNode!.next
+            currentIndex += 1
+        }
+        
+        return currentNode
+    }
+    
+    public mutating func push(_ value: Value) {
+        copyNodes()
+        head = Node(value: value, next: head)
+        if tail == nil {
+            tail = head
+        }
+    }
+    
+    public mutating func append(_ value: Value) {
+        copyNodes()
+        guard !isEmpty else {
+            push(value)
+            return
+        }
+        
+        tail!.next = Node(value: value, next: nil)
+        tail = tail!.next
+    }
+    
+    public mutating func insert(at index: Int, value: Value) {
+        copyNodes()
+        // 链表为空，直接压入
+        if isEmpty {
+            push(value)
+        } else {
+            // 表头插入
+            guard index > 0 else {
+                push(value)
+                return
+            }
+            // 表尾添加
+            guard node(at: index) != nil else {
+                append(value)
+                return
+            }
+            // 表中插入
+            let prev = node(at: index - 1)
+            prev!.next = Node(value: value, next: prev!.next)
+        }
+    }
+    
+    @discardableResult
+    public mutating func insert(_ value: Value, after node: Node<Value>) -> Node<Value>? {
+      guard let node = copyNodes(returningCopyOf: node) else { return nil }
+      guard tail !== node else {
+        append(value)
+        return tail!
+      }
+      node.next = Node(value: value, next: node.next)
+      return node.next!
+    }
+    
+    public mutating func pop() -> Value? {
+        copyNodes()
+        defer {
+            head = head?.next
+            if isEmpty {
+                tail = nil
+            }
+        }
+        return head?.value
+    }
+    
+    @discardableResult
+    public mutating func removeLast() -> Value? {
+        copyNodes()
+        // 空
+        guard let head = head else {
+            return nil
+        }
+        
+        // 只有一个结点
+        guard head.next != nil else {
+            return pop()
+        }
+        
+        var prev = head
+        var current = head
+        
+        while let next = current.next {
+            prev = current
+            current = next
+        }
+        
+        prev.next = nil
+        tail = prev
+        
+        return current.value
+    }
+    
+    @discardableResult
+    public mutating func remove(at index: Int) -> Value? {
+        copyNodes()
+        if isEmpty {
+            return nil
+        }
+        
+        // 移除表头
+        guard index > 0 else {
+            defer {
+                head = head!.next
+                if head == nil {
+                    tail = nil
+                }
+            }
+            return head!.value
+        }
+        
+        guard let prev = node(at: index - 1) else {
+            return nil
+        }
+
+        guard let current = prev.next else {
+            return nil
+        }
+        
+        guard let prevNext = current.next else {
+            // 移除表尾
+            prev.next = nil
+            tail = prev
+            return current.value
+        }
+        
+        prev.next = prevNext
+        
+        return current.value
+    }
+    
+    @discardableResult
+    public mutating func remove(after node: Node<Value>) -> Value? {
+        // 这里移除的 node 是原先链表里的 node，而使用 copyNodes 之后创建了新的一个链表
+        // 所以移除的 node 是旧链表的结点
+        // copyNodes()
+        // 在创建新链表的过程中，找到与 node 对应的结点并返回到这里
+        guard let node = copyNodes(returningCopyOf: node) else { return nil }
+        defer {
+            if node.next === tail {
+                tail = node
+            }
+            node.next = node.next?.next
+        }
+        return node.next?.value
+    }
+    
+    private mutating func copyNodes() {
+        guard !isKnownUniquelyReferenced(&head) else {
+          return
+        }
+        
+        guard var oldNode = head else {
+            return
+        }
+        
+        head = Node(value: oldNode.value)
+        var newNode = head
+        
+        while let nextOldNode = oldNode.next {
+            newNode!.next = Node(value: nextOldNode.value)
+            newNode = newNode!.next
+            
+            oldNode = nextOldNode
+        }
+        
+        tail = newNode
+    }
+    
+    private mutating func copyNodes(returningCopyOf node: Node<Value>?) -> Node<Value>? {
+      guard !isKnownUniquelyReferenced(&head) else {
+          return node
+      }
+      guard var oldNode = head else {
+        return nil
+      }
+
+      head = Node(value: oldNode.value)
+      var newNode = head
+      var nodeCopy: Node<Value>?
+
+      while let nextOldNode = oldNode.next {
+        if oldNode === node {
+          nodeCopy = newNode
+        }
+        newNode!.next = Node(value: nextOldNode.value)
+        newNode = newNode!.next
+        oldNode = nextOldNode
+      }
+      
+        if oldNode === node {
+            nodeCopy = newNode
+        }
+      tail = newNode
+      return nodeCopy
+    }
+}
+
+extension LinkedList: CustomStringConvertible {
+    public var description: String {
+        guard let head = head else {
+            return "Empty list"
+        }
+        return String(describing: head)
+    }
+}
+
+extension LinkedList: Collection {
+    public struct Index: Comparable {
+        public var node: Node<Value>?
+
+        static public func ==(lhs: Index, rhs: Index) -> Bool {
+            switch (lhs.node, rhs.node) {
+            case let (left?, right?):
+                return left.next === right.next
+            case (nil, nil):
+                return true
+            default:
+                return false
+            }
+        }
+
+        static public func <(lhs: Index, rhs: Index) -> Bool {
+            guard lhs != rhs else {
+                return false
+            }
+            let nodes = sequence(first: lhs.node) { $0?.next }
+            /// 什么意思
+            return nodes.contains { $0 === rhs.node }
+        }
+    }
+
+    public var startIndex: Index {
+        Index(node: head)
+    }
+
+    public var endIndex: Index {
+        Index(node: tail?.next)
+    }
+
+    public func index(after i: Index) -> Index {
+        Index(node: i.node?.next)
+    }
+
+    public subscript(position: Index) -> Value {
+        position.node!.value
+    }
+}
--- Data Structures & Alogrithms in Swift/Data Structures & Alogrithms in Swift/main.swift
+++ Data Structures & Alogrithms in Swift/Data Structures & Alogrithms in Swift/main.swift
@@ -31,3 +31,62 @@ stack2.pop()
 
 var stack3 = [1.1, 2.2, 3.3]
 print(stack3)
+
+var list1 = LinkedList<Int>()
+list1.append(1)
+list1.append(2)
+print("List1 uniquely referenced: \(isKnownUniquelyReferenced(&list1.head))")
+var list2 = list1
+print("List1 uniquely referenced: \(isKnownUniquelyReferenced(&list1.head))")
+
+
+print("List1: \(list1)")
+print("List2: \(list2)")
+
+print("After appending 3 to list2")
+list2.append(3)
+print("List1: \(list1)")
+print("List2: \(list2)")
+  
+print("Removing middle node on list2")
+if let node = list2.node(at: 0) {
+    list2.remove(after: node)
+}
+list2.append(4)
+print("List1: \(list1)")
+print("List2: \(list2)")
+
+var list3 = LinkedList<Int>()
+(1...3).forEach { list3.append($0) }
+var list4 = list3
+list4.push(0)
+print("List3: \(list3)")
+print("List4: \(list4)")
+list3.push(100)
+print("List3: \(list3)")
+print("List4: \(list4)")
+list4.append(4)
+print("List3: \(list3)")
+print("List4: \(list4)")
+list3.append(5)
+print("List3: \(list3)")
+print("List4: \(list4)")
+list3.insert(at: 1, value: 200)
+print("List3: \(list3)")
+print("List4: \(list4)")
+list4.insert(at: 3, value: 839)
+print("List3: \(list3)")
+print("List4: \(list4)")
+
+var list5 = LinkedList<Int>()
+(1...3).forEach { list5.append($0) }
+var list6 = list5
+print("List5: \(list5)")
+print("List6: \(list6)")
+list5.insert(384, after: list5.node(at: 2)!)
+print("List5: \(list5)")
+print("List6: \(list6)")
+list6.insert(558, after: list6.node(at: 1)!)
+print("List5: \(list5)")
+print("List6: \(list6)")
+
